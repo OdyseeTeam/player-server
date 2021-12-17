668286b8305b7c570c7802e71207749273a25a20
Niko Storni <niko@odysee.com>
2021-12-17 21:10:30 +0100

    replace net/http with gin-go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,61 +1,44 @@
 package config
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/lbryio/lbrytv-player/player"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
 var UserName string
 var Password string
 
-func InstallConfigRoute(r *mux.Router) {
-	r.Handle("/config/throttle", basicAuthWrapper(throttle()))
+func InstallConfigRoute(r *gin.Engine) {
+	authorized := r.Group("/config", gin.BasicAuth(gin.Accounts{
+		UserName: Password,
+	}))
+	authorized.POST("/config/throttle", throttle)
 }
 
 //throttle allows for live configuration of the throttle scalar for the player (MB/s)
 //http://localhost:8080/config/throttle?scale=1.2    //postman to add basic auth or temporarily remove basic auth
-func throttle() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		enabledStr := r.FormValue("enabled")
-		if enabledStr != "" {
-			enabled, err := strconv.ParseBool(enabledStr)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintf(w, "failed to parse enabled %s", enabledStr)
-			} else {
-				player.ThrottleSwitch = enabled
-			}
-		}
-		scaleStr := r.FormValue("scale")
-		if scaleStr != "" {
-			scale, err := strconv.ParseFloat(scaleStr, 64)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintf(w, "failed to parse scale %s", scaleStr)
-			} else {
-				player.ThrottleScale = scale
-			}
-		}
-	})
-}
-
-//basicAuthWrapper is a wrapper function to ensure some authentication for accessing an API
-func basicAuthWrapper(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		user, pass, ok := r.BasicAuth()
-		if !ok {
-			http.Error(w, "authentication required", http.StatusBadRequest)
+func throttle(c *gin.Context) {
+	enabledStr := c.PostForm("enabled")
+	if enabledStr != "" {
+		enabled, err := strconv.ParseBool(enabledStr)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "failed to parse enabled " + enabledStr})
 			return
-		}
-		if user == UserName && pass == Password {
-			h.ServeHTTP(w, r)
 		} else {
-			http.Error(w, "invalid username or password", http.StatusForbidden)
+			player.ThrottleSwitch = enabled
 		}
-	})
+	}
+	scaleStr := c.PostForm("scale")
+	if scaleStr != "" {
+		scale, err := strconv.ParseFloat(scaleStr, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "failed to parse scale " + scaleStr})
+		} else {
+			player.ThrottleScale = scale
+		}
+	}
 }
--- player/http_routes.go
+++ player/http_routes.go
@@ -1,45 +1,43 @@
 package player
 
 import (
-	"net/http"
-	"net/http/pprof"
-
-	"github.com/gorilla/mux"
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
 )
 
 const ProfileRoutePath = "/superdebug/pprof"
 
-func InstallPlayerRoutes(r *mux.Router, p *Player) {
+func InstallPlayerRoutes(r *gin.Engine, p *Player) {
 	playerHandler := NewRequestHandler(p)
 
-	v1Router := r.Path("/content/claims/{claim_name}/{claim_id}/{filename}").Subrouter()
-	v1Router.HandleFunc("", playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
+	v1Router := r.Group("/content/claims/:claim_name/:claim_id/:filename")
+	v1Router.GET("", playerHandler.Handle)
+	v1Router.HEAD("", playerHandler.Handle)
 
-	v2Router := r.PathPrefix("/api/v2").Subrouter()
-	v2Router.Path("/streams/free/{claim_name}/{claim_id}").HandlerFunc(playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
-	v2Router.Path("/streams/paid/{claim_name}/{claim_id}/{token}").HandlerFunc(playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
+	v2Router := r.Group("/api/v2")
+	v2Router.GET("/streams/free/:claim_name/:claim_id", playerHandler.Handle)
+	v2Router.HEAD("/streams/free/:claim_name/:claim_id", playerHandler.Handle)
+	v2Router.GET("/streams/paid/:claim_name/:claim_id/:token", playerHandler.Handle)
+	v2Router.HEAD("/streams/paid/:claim_name/:claim_id/:token", playerHandler.Handle)
 
-	v3Router := r.PathPrefix("/api/v3").Subrouter()
-	v3Router.Path("/streams/free/{claim_name}/{claim_id}/{sd_hash}").HandlerFunc(playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
-	v3Router.Path("/streams/paid/{claim_name}/{claim_id}/{sd_hash}/{token}").HandlerFunc(playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
+	v3Router := r.Group("/api/v3")
+	v3Router.GET("/streams/free/:claim_name/:claim_id/:sd_hash", playerHandler.Handle)
+	v3Router.HEAD("/streams/free/:claim_name/:claim_id/:sd_hash", playerHandler.Handle)
+	v3Router.GET("/streams/paid/:claim_name/:claim_id/:sd_hash/:token", playerHandler.Handle)
+	v3Router.HEAD("/streams/paid/:claim_name/:claim_id/:sd_hash/:token", playerHandler.Handle)
 
-	v4Router := r.PathPrefix("/api/v4").Subrouter()
-	v4Router.Path("/streams/free/{claim_name}/{claim_id}/{sd_hash}").HandlerFunc(playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
+	v4Router := r.Group("/api/v4")
+	v4Router.GET("/streams/free/:claim_name/:claim_id/:sd_hash", playerHandler.Handle)
+	v4Router.HEAD("/streams/free/:claim_name/:claim_id/:sd_hash", playerHandler.Handle)
 
-	r.PathPrefix(SpeechPrefix).HandlerFunc(playerHandler.Handle).Methods(http.MethodGet, http.MethodHead)
+	r.GET(SpeechPrefix, playerHandler.Handle)
+	r.HEAD(SpeechPrefix, playerHandler.Handle)
 
 	if p.TCVideoPath != "" {
-		v4Router.Path("/streams/tc/{claim_name}/{claim_id}/{sd_hash}/{fragment}").HandlerFunc(playerHandler.HandleTranscodedFragment)
+		v4Router.GET("/streams/tc/:claim_name/:claim_id/:sd_hash/:fragment", playerHandler.HandleTranscodedFragment)
 	}
 }
 
-func InstallProfilingRoutes(r *mux.Router) {
-	debugRouter := r.PathPrefix(ProfileRoutePath).Subrouter()
-	debugRouter.HandleFunc("/", pprof.Index)
-	debugRouter.Handle("/goroutine", pprof.Handler("goroutine"))
-	debugRouter.HandleFunc("/cmdline", pprof.Cmdline)
-	debugRouter.HandleFunc("/profile", pprof.Profile)
-	debugRouter.HandleFunc("/symbol", pprof.Symbol)
-	debugRouter.HandleFunc("/trace", pprof.Trace)
-	debugRouter.Handle("/heap", pprof.Handler("heap"))
+func InstallProfilingRoutes(r *gin.Engine) {
+	pprof.Register(r, ProfileRoutePath)
 }
