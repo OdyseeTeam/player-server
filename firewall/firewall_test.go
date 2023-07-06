bc3ceeeeabf393492f9babe22abad55bf208bc08
Niko Storni <niko@odysee.com>
2023-07-07 01:37:08 +0200

    fix test

--- firewall/firewall_test.go
+++ firewall/firewall_test.go
@@ -12,14 +12,14 @@ func TestCheckIPAccess(t *testing.T) {
 
 	// Test the first five accesses for an IP don't exceed the limit
 	for i := 1; i <= 5; i++ {
-		result := IsIpAbusingResources(ip, endpoint+strconv.Itoa(i))
+		result, _ := IsIpAbusingResources(ip, endpoint+strconv.Itoa(i))
 		if result {
 			t.Errorf("Expected result to be false, got %v for endpoint %s", result, endpoint+strconv.Itoa(i))
 		}
 	}
 
 	// Test the sixth access for an IP exceeds the limit
-	result := IsIpAbusingResources(ip, endpoint+"6")
+	result, _ := IsIpAbusingResources(ip, endpoint+"6")
 	if !result {
 		t.Errorf("Expected result to be true, got %v for endpoint %s", result, endpoint+"6")
 	}
@@ -28,7 +28,7 @@ func TestCheckIPAccess(t *testing.T) {
 	time.Sleep(WindowSize)
 
 	// Test the access for an IP after the window size elapses doesn't exceed the limit
-	result = IsIpAbusingResources(ip, endpoint+"7")
+	result, _ = IsIpAbusingResources(ip, endpoint+"7")
 	if result {
 		t.Errorf("Expected result to be false, got %v for endpoint %s", result, endpoint+"7")
 	}
